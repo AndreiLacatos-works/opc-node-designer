146b81f2c723ffad74f3c8b08ab8f4ef71859e9a
AndreiLacatos <andrei_lacatos@yahoo.com>
2024-11-24 16:57:17 +0200

    Bootstrap blank flutter app

---
+++ src/waveform_designer/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+  await windowManager.setTitle('Waveform designer');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WidgetsApp(
+      title: 'Waveform designer',
+      color: Color.fromARGB(255, 45, 45, 45),
+      home: Container(
+        decoration: const BoxDecoration(color: Color.fromARGB(255, 45, 45, 45)),
+        child: null,
+      ),
+      pageRouteBuilder: <T>(RouteSettings settings, WidgetBuilder builder) =>
+          PageRouteBuilder<T>(
+        settings: settings,
+        pageBuilder: (BuildContext context, Animation<double> animation,
+                Animation<double> secondaryAnimation) =>
+            builder(context),
+      ),
+    );
+  }
+}
