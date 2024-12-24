a9260e5324f134fee90fdbee805785ae90e480eb
AndreiLacatos <andrei_lacatos@yahoo.com>
2024-12-24 12:20:26 +0200

    Minor bug fix

--- src/waveform_designer/lib/widgets/designer/chart/InteractionHandler.dart
+++ src/waveform_designer/lib/widgets/designer/chart/InteractionHandler.dart
@@ -102,7 +102,7 @@ class _InteractionHandler extends ConsumerState<InteractionHandler>
             );
       }
     } else {
-      var delta = (_dragStartXOffset ?? 0.0 - details.localPosition.dx).abs();
+      var delta = ((_dragStartXOffset ?? 0.0) - details.localPosition.dx).abs();
       // prevent zoom when selected area is really small (could be accidentally
       // 1 or 2 pixels when clicking)
       if (_dragStartXOffset != null && delta > 12) {
