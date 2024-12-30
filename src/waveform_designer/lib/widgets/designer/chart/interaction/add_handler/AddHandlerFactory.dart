36eb53304134cae89e7ae73b8766b6e1119a96ac
AndreiLacatos <andrei_lacatos@yahoo.com>
2024-12-30 16:57:55 +0200

    Fix handler factory exception

--- src/waveform_designer/lib/widgets/designer/chart/interaction/add_handler/AddHandlerFactory.dart
+++ src/waveform_designer/lib/widgets/designer/chart/interaction/add_handler/AddHandlerFactory.dart
@@ -1,11 +1,13 @@
 import 'package:waveform_designer/state/waveform/waveform.model.dart';
 import 'package:waveform_designer/widgets/designer/chart/interaction/add_handler/AbstractValueAddHandler.dart';
+import 'package:waveform_designer/widgets/designer/chart/interaction/add_handler/NullAddHandler.dart';
 import 'package:waveform_designer/widgets/designer/chart/interaction/add_handler/NumericValueAddHandler.dart';
 import 'package:waveform_designer/widgets/designer/chart/interaction/add_handler/TransitionValueAddHandler.dart';
 
 class AddHandlerFactory {
   static AbstractValueAddHandler getAddHandler(WaveFormModel waveform) {
     return switch (waveform.type) {
+      Void => NullAddHandler(),
       Transition => TransitionValueAddHandler(),
       DoubleValue => NumericValueAddHandler(),
       _ => throw "${waveform.type} has no add handler",
